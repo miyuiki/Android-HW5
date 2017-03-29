442c8876c82848c8c912128836521e2cd7eb1053
Chang Jay <a121406@gmail.com>
2017-03-29 12:30:44 +0800

    0329 commited from PC

--- app/src/main/java/com/example/jay/formhw/MainActivity.java
+++ app/src/main/java/com/example/jay/formhw/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.jay.formhw;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    private Spinner spnStart;
+    private Spinner spnEnd;
+    private Spinner spnAd;
+    private Spinner spnCh;
+    String[] station = new String[] {"南港","台北","板橋","桃園","新竹","台中","嘉義","台南","左營"};
+    String[] ticket = new String[] {"0","1","2","3","4","5"};
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spnStart = (Spinner) findViewById(R.id.spinner);
+        spnEnd = (Spinner) findViewById(R.id.spinner2);
+        spnAd = (Spinner) findViewById(R.id.spinner3);
+        spnCh = (Spinner) findViewById(R.id.spinner4);
+
+        ArrayAdapter<String> adapterStation = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,station);
+        adapterStation.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<String> adapterTicket = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,ticket);
+        adapterTicket.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spnStart.setAdapter(adapterStation);
+        spnEnd.setAdapter(adapterStation);
+        spnAd.setAdapter(adapterTicket);
+        spnCh.setAdapter(adapterTicket);
+
     }
 }
